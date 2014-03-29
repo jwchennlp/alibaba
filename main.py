eab5edaf4fbdac47fb942599df8b261a186c7f90
jwchennlp <hit1093710417@gmail.com>
2014-03-29 22:57:59 +0800

    set the min M for the restrict of whether rec a item

--- main.py
+++ main.py
@@ -38,21 +38,22 @@ def func3():
 
 
 #交叉验证
-def cross_valation(K,T,train_u_i_t,test_u_i):
+def cross_valation(K,T,train_u_i_t,test_u_i,M):
     day_count = 91
     #获取训练集的推荐列表
     rec_user_item = usermodel.build_user_model(train_u_i_t,T,day_count)
     #需要对推荐列表进行处理，对每个用户设定推荐的物品的个数,K为设定的物品的个数
-    rec_user_item = func.set_rec_item_num(rec_user_item,K)
+    rec_user_item = func.set_rec_item_num(rec_user_item,K,M)
     #计算准确率，召回率，F值等参数
     hit_user_item = func.cal_hit_user_item(rec_user_item,test_u_i) 
     precision,recall,F,hitbrand,pbrand,bbrand = func.cal_result(rec_user_item,test_u_i,hit_user_item)
     
-    func.write(rec_user_item,'train_rec_user_item')
     func.write(hit_user_item,'hit_user_item')
+    func.write(rec_user_item,'train_rec_user_item')
     
     print '限定对每个用户推荐的物品数为:',K
     print '时间衰减因此设定为（越小衰减越快）',T
+    print '限定对物品的最小推荐值',M
     print 'hitbrand',hitbrand,'pbrand',pbrand,'precision',precision
     print 'hitbrand',hitbrand,'bbrand',bbrand,'recall',recall
     print 'F',F
@@ -60,22 +61,26 @@ def cross_valation(K,T,train_u_i_t,test_u_i):
 def train():
     train_u_i_t = func.read('train_user_item_time')
     test_u_i = func.read('test_user_item')
-    Klist = [5]
-    Tlist = [8]
+    Klist = [4]
+    Mlist = [0.9]
+    Tlist = [12]
     for K in Klist:
         for T in Tlist:
-            cross_valation(K,T,train_u_i_t,test_u_i)
+            for M in Mlist:
+                cross_valation(K,T,train_u_i_t,test_u_i,M)
     
     
 def func4():
     K = 5
-    T=8
+    T=12
+    M=0.9
     day_count = 123
     user_item_time = func.read('user_item_time')
     rec_user_item = usermodel.build_user_model(user_item_time,T,day_count)
-    rec_user_item = func.set_rec_item_num(rec_user_item,K)
+    rec_user_item = func.set_rec_item_num(rec_user_item,K,M)
     #查看为用户推荐物品的个数分布,对有那些推荐物品个数小于等于3的用户给其推荐热门商品
-    rec_user_item=func.distribution(rec_user_item)
+    func.look_distribuction(rec_user_item,K)
+    #rec_user_item=func.distribution(rec_user_item)
     func.write(rec_user_item,'rec_user_item')
     
     func.getresult(rec_user_item)
@@ -83,7 +88,7 @@ def func4():
     
 
 if __name__=="__main__":
-    func4()
+    train()
     
     
     
